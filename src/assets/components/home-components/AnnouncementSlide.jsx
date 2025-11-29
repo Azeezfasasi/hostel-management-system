06117e89e4ce08253fd0e4e9b212901106bac403
Calculus247 <orinyat@gmail.com>
2025-11-29 22:03:56 +0100

    color change in announcement slide

--- src/assets/components/home-components/AnnouncementSlide.jsx
+++ src/assets/components/home-components/AnnouncementSlide.jsx
@@ -47,7 +47,7 @@ export default function AnnouncementSlide() {
   };
 
   return (
-    <div className="py-3 bg-gradient-to-r from-red-400 to-red-600">
+    <div className="py-3 bg-gradient-to-r from-snow-400 to-snow-600">
       <style>{`
         @keyframes slideLeft {
           0% { transform: translateX(100%); opacity: 0; }
